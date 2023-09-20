71c27fb5bb724df58e70c38b2fc23bdffdb0f30e
olonakalie <131878955+olonakalie@users.noreply.github.com>
2023-09-20 17:41:38 +0200

    simpleProject
    simpleProject

---
+++ SimplemProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyCSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, there!");
+
+            Console.WriteLine("How are doing?");
+            string firstQ = Console.ReadLine();
+
+            Console.WriteLine("Thanks you for letting me know that you are " + firstQ);
+        }
+    }
+}
+
